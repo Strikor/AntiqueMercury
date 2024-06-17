3acd535d10627036bb425e613790ca2d404a9ea9
Jason <jasongraham8231@gmail.com>
2024-06-17 12:20:38 -0400

    Initial Commit
    A working base to work from

---
+++ Player/player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+signal hit
+
+@export var speed = 300.0
+@export var jump_velocity = -400.0
+var screen_size # Size of the game window
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") :
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
